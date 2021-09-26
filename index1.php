5db851a130da4d7512f4fcf99f4642c8054b417e
Saberzhan19 <Saberzhax@gmail.com>
2021-09-26 19:36:23 +0600

    style change

--- index1.php
+++ index1.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
@@ -23,8 +23,8 @@
     ?>
 
     <?php foreach ($names as $name):?>
-        <h4><?php echo $name["name"]; ?></h4>-
-        <p><?php echo $name["role"]; ?></p><br>
+        <h4><?php echo $name["name"]; ?></h4>
+        <p><?php echo $name["role"]; ?></p>
     <?php endforeach;?>
 
 
